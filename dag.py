ea070b30d08f11b6174ec4b6fa20eb93b938bcc3
mfernandezlabastida <mfernandezlabastida@ikerlan.es>
2024-09-17 16:53:38 +0200

    change on task conf

--- dag.py
+++ dag.py
@@ -59,7 +59,8 @@ def DAG_image_build_REST():
             requests={'cpu': '0.5'},
             limits={'cpu': '0.8'}
         ),
-        env_vars=env_vars
+        env_vars=env_vars,
+        provide_context=True
     )
     def image_build_task(**kwargs):
         import logging
