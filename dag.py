ddecfa0ade7d54584849e06d36cdfec54ed0ff41
mfernandezlabastida <mfernandezlabastida@ikerlan.es>
2024-09-24 16:49:44 +0200

    testing for fix requirements error

--- dag.py
+++ dag.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from airflow.decorators import dag, task
+from airflow.decorators import dag
 from kubernetes.client import models as k8s
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.models import Variable
@@ -19,9 +19,8 @@ def DAG_image_build_REST():
     requirements = "{{ dag_run.conf.get('requirements') }}"
     python_version = "{{ dag_run.conf.get('python_version') }}"
     use_gpu = "{{ dag_run.conf.get('use_gpu') }}"
-    # print(requirements, user, password, endpoint)
 
-    env_vars={
+    env_vars = {
         "POSTGRES_USERNAME": Variable.get("POSTGRES_USERNAME"),
         "POSTGRES_PASSWORD": Variable.get("POSTGRES_PASSWORD"),
         "POSTGRES_DATABASE": Variable.get("POSTGRES_DATABASE"),
@@ -44,22 +43,34 @@ def DAG_image_build_REST():
         "use_gpu": use_gpu
     }
 
-    volume_mount = k8s.V1VolumeMount(
-        name="dag-dependencies", mount_path="/git"
+    # Define el volumen para compartir entre initContainer y el container principal
+    volume = k8s.V1Volume(name="docker-config", empty_dir=k8s.V1EmptyDirVolumeSource())
+
+    # Init container para crear el archivo config.json
+    init_container = k8s.V1Container(
+        name="create-config",
+        image="alpine:latest",  # Usar una imagen ligera
+        command=["sh", "-c"],
+        args=[f"echo '{{\"auths\": {{\"https://index.docker.io/v1/\": {{\"auth\": \"$(echo -n {user}:{password} | base64)\"}}}}}}' > /config/config.json"],
+        volume_mounts=[k8s.V1VolumeMount(mount_path="/config", name="docker-config")]
     )
 
-    init_container_volume_mounts = [
-        k8s.V1VolumeMount(mount_path="/git", name="dag-dependencies")
-    ]
-
-    volume = k8s.V1Volume(name="dag-dependencies", empty_dir=k8s.V1EmptyDirVolumeSource())
-
-    init_container = k8s.V1Container(
+    # Contenedor para clonar el repositorio
+    git_clone_container = k8s.V1Container(
         name="git-clone",
         image="alpine/git:latest",
         command=["sh", "-c", "mkdir -p /git && cd /git && git clone -b main --single-branch https://github.com/MiKeLFernandeZz/Img_build_rest.git"],
-        volume_mounts=init_container_volume_mounts
+        volume_mounts=[k8s.V1VolumeMount(mount_path="/git", name="dag-dependencies")]
     )
+
+    # Montaje del volumen en el pod de Kaniko
+    volume_mount = k8s.V1VolumeMount(
+        name="docker-config",
+        mount_path="/kaniko/.docker/"  # Este es el path que Kaniko espera para config.json
+    )
+
+    # Crear el volumen para las dependencias de git
+    dag_dependencies_volume = k8s.V1Volume(name="dag-dependencies", empty_dir=k8s.V1EmptyDirVolumeSource())
 
     path = '/git/Img_build_rest/docker'
 
@@ -69,17 +80,15 @@ def DAG_image_build_REST():
         namespace='airflow',
         image='gcr.io/kaniko-project/executor:latest',
         env_vars=env_vars,
-        init_containers=[init_container],
-        volumes=[volume],
-        volume_mounts=[volume_mount],
+        init_containers=[init_container, git_clone_container],  # Añadir ambos init containers
+        volumes=[volume, dag_dependencies_volume],
+        volume_mounts=[volume_mount, k8s.V1VolumeMount(mount_path="/git", name="dag-dependencies")],
         cmds=["/kaniko/executor"],
         arguments=[
             f"--dockerfile={path}/Dockerfile",
             f"--context={path}",
             f"--destination={endpoint}",
-            f"--build-arg=PYTHON_VERSION={python_version}",
-            f"--build-arg=DOCKER_USERNAME={user}",
-            f"--build-arg=DOCKER_PASSWORD={password}"
+            f"--build-arg=PYTHON_VERSION={python_version}"
         ]
     )
 
