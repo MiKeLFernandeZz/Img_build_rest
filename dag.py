1785eb0d0eee40e76ec7c4989a49fe58eae04d23
mfernandezlabastida <mfernandezlabastida@ikerlan.es>
2024-09-24 16:27:13 +0200

    testing for fix requirements error

--- dag.py
+++ dag.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from airflow.decorators import dag, task
 from kubernetes.client import models as k8s
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow.models import Variable
 
 @dag(
